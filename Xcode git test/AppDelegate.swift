b35fa7440f1280234f83cd5f46704262a88dd645
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-08-13 13:06:38 -0500

    Initial Commit

---
+++ Xcode git test/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Xcode git test
+//
+//  Created by Laurie McGee on 8/13/17.
+//  Copyright © 2017 Laurie McGee. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
